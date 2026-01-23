c5e10479cab4fcf1023b6be5978b33fd8baa10df
Carl Timothy Ligutom <carl.timothy.ligutom@student.uno-r.edu.ph>
2026-01-23 03:17:56 +0000

    End of day 1.

--- Jing.java
+++ Jing.java
@@ -1,8 +1,8 @@
 class Jing {
     void main() {
-        System.out.println("(          )");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("  O");
+        System.out.println("  |");
+        System.out.println("  |");
         System.out.println("");
         System.out.println("");
         System.out.println("");
